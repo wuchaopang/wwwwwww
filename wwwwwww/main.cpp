37c125b385aa619857a7b06c909311301d92c7b9
wuchaopang <20141105049wch@mac23.local>
2015-11-11 19:38:23 +0800

    Initial Commit

---
+++ wwwwwww/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  wwwwwww
+//
+//  Created by 20141105049 on 15/11/11.
+//  Copyright © 2015年 20141105049. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
