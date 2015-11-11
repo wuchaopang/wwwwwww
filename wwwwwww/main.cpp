12a00eda78137109f3cd5798f9dd5b4496a0ca6b
wuchaopang <20141105049wch@mac23.local>
2015-11-11 19:40:18 +0800

    第三次提交
    指针和数组

--- wwwwwww/main.cpp
+++ wwwwwww/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  wwwwwww
+//  wwu
 //
 //  Created by 20141105049 on 15/11/11.
 //  Copyright © 2015年 20141105049. All rights reserved.
@@ -9,7 +9,15 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[4];
+    int *p;
+    a[0]=10;
+    a[1]=20;
+    a[2]=30;
+    a[3]=40;
+    p=a;
+    printf("&a[0]=%x,&a[1]=%x,&a[2]=%x,&[3]=%x,a=%x,*p=%x\n",a[0],a[1],a[2],a[3],a,p);
+    
+    
     return 0;
 }
